4343fcdbddce31cbcfcbce66c23fb0b01e8916b5
Jason Brown <jason.brown91@outlook.com>
2020-04-27 14:03:45 -0600

    Finished Sales Header for now

--- client/src/components/Views/Sales/Sales.js
+++ client/src/components/Views/Sales/Sales.js
@@ -19,15 +19,24 @@ const Sales = (props) => {
         const salesObj = {
             activeSales: [0, 0],
             YTDProfit: 0,
+            allItemsProfit: 0,
+            profitPerItem: 0,
+            totalSold: 0,
+            totalListed: 0
         }
 
         //Once I add expenses I will be able to implement that into my YTD Profit
         const info = items.reduce((salesInfo, x) => {
             if (x.sold) {
                 salesInfo.YTDProfit += x.profit;
+                salesInfo.allItemsProfit += x.profit;
+                salesInfo.totalSold++
+                salesInfo.profitPerItem = "$" + (salesInfo.allItemsProfit / salesInfo.totalSold).toFixed(2); 
             } else {
                 salesInfo.activeSales[0] += x.listedPrice;
                 salesInfo.activeSales[1] += x.expectedProfit;
+                salesInfo.totalListed++
+                
             }
 
             return salesInfo;
--- client/src/components/Views/Sales/SalesHeader/SalesHeader.js
+++ client/src/components/Views/Sales/SalesHeader/SalesHeader.js
@@ -2,18 +2,27 @@ import React from "react";
 import Styles from "./SalesHeader.module.scss";
 
 const SalesHeader = ({salesInfo}) => {
-    const { activeSales, YTDProfit } = salesInfo
+    const { activeSales, YTDProfit, profitPerItem, totalListed, totalSold } = salesInfo
     return (
         <div className={Styles.wrapper}>
-            <h4>Active sales 
-                <span>{`  $${activeSales[0].toFixed(2)} /`}
-                    <span style={{color: "green"}}>{`$${activeSales[1].toFixed(2)}`}
+            <h5>Listed 
+                <span>{totalListed}</span>
+            </h5>
+            <h5>Sold 
+                <span>{totalSold}</span>
+            </h5>
+            <h5>Active sales 
+                <span>{`$${activeSales[0].toFixed(2)} /`}
+                    <span style={{color: "green", display: "inline"}}>{`$${activeSales[1].toFixed(2)}`}
                     </span>
                 </span>
-            </h4>
-            <h4>YTD Profit 
+            </h5>
+            <h5>Avg profit/item 
+                <span style={{color: "green"}}>{profitPerItem}</span>
+            </h5>
+            <h5>YTD Profit 
                 <span style={{color: "green"}}>{`$${YTDProfit.toFixed(2)}`}</span>
-            </h4>
+            </h5>
             
         </div>
     );
