363b453172c6264122d4fbd5d08d53ecd26fb162
Jason Brown <jason.brown91@outlook.com>
2020-09-20 08:28:55 -0600

    Making changes to the template

--- client/src/components/Views/Inventory/NewItemModal/ListingForm/ListingForm.js
+++ client/src/components/Views/Inventory/NewItemModal/ListingForm/ListingForm.js
@@ -13,7 +13,7 @@ const ListingForm = (props) => {
         conditionDescription: "",
         acceptOfferHigh: "",
         declineOfferLow: "",
-        description: "Please double check the part number you are looking for to be sure this part is compatible with your vehicle. Some ECU’s (Engine Control Unit) need to be reprogrammed with your vehicle's VIN. This process is not done by me. Please research the specific process your vehicle’s ECU may need before purchasing this ECU.\n\nThank you!",
+        description: "Please double check the part number you are looking for and do your own research to be sure this part is compatible with your vehicle. Some ECU’s (Engine Control Unit) need to be reprogrammed with your vehicle's VIN. This process is not done by me. Please research the specific process your vehicle’s ECU may need before purchasing this ECU.\n\nThank you!",
         location: "",
     })
 
