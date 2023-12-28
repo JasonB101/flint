be966dbd74d247ac8dff805416a96eefcaab79b2
JasonB101 <jason.brown91@outlook.com>
2023-12-28 15:48:40 -0700

    Added info to active listings and sorted it by listing date

--- client/src/components/Views/Inventory/NewItemModal/ListingForm/ActiveListingsModal/ActiveListingsModal.js
+++ client/src/components/Views/Inventory/NewItemModal/ListingForm/ActiveListingsModal/ActiveListingsModal.js
@@ -2,16 +2,34 @@ import React from 'react'
 import Styles from './ActiveListingsModal.module.scss' // Import your SCSS file for styling
 
 const ActiveListingsModal = ({ activeListings, closeActiveListingsModal }) => {
+  if (activeListings.length > 0) {
+    activeListings.sort((a, b) => dateToTime(b.listedOn) - dateToTime(a.listedOn))
+  }
+  const listingInfo = activeListings.reduce((info, item) => {
+      info.sum += +item.price
+      info.highest = +item.price > info.highest ? +item.price : info.highest
+      info.lowest = +item.price < info.lowest || info.lowest === 0 ? +item.price : info.lowest
+      return info
+  }, {sum: 0, highest: 0, lowest: 0})
+
+  listingInfo.average = activeListings.length > 0 ? Math.floor(listingInfo.sum / activeListings.length) : 0
 
   return (
     <div className={Styles.modal}>
       <div className={Styles.modalContent}>
         <div className={Styles.modalHeader}>
-          <h2>eBay Listings</h2>
+          <div className='spacer'></div>
           <span className={Styles.closeModal} onClick={closeActiveListingsModal}>
             &times;
           </span>
+          
         </div>
+        <div className={Styles['listingInfo']}>
+            <p>{`Average: $${listingInfo.average}`}</p>
+            <p>{`Highest: $${listingInfo.highest}`}</p>
+            <p>{`Lowest: $${listingInfo.lowest}`}</p>
+            <p>{`Quantity: ${activeListings.length}`}</p>
+          </div>
         <div className={Styles.modalBody}>
           {activeListings.map((item) => (
             <div key={item.itemId} className={Styles.itemContainer}>
@@ -41,4 +59,7 @@ const ActiveListingsModal = ({ activeListings, closeActiveListingsModal }) => {
   )
 }
 
+function dateToTime(value) {
+  return String(new Date(value).getTime())
+}
 export default ActiveListingsModal
