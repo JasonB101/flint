76b5343b6510f6fe6a515f8c9d36fc78aa6f6a51
JasonB101 <jason.brown91@outlook.com>
2025-01-20 14:07:22 -0700

    Set churn days to account for today being 0

--- client/src/components/Views/Churn/Churn.js
+++ client/src/components/Views/Churn/Churn.js
@@ -7,7 +7,7 @@ const Churn = ({ churnSettings, saveChurnSettings, items }) => {
   const [churnDaysToShow, setChurnDaysToShow] = useState(churnSettings?.churnDaysToShow || 3)
   
   const windowOfDays = new Date()
-  windowOfDays.setDate(windowOfDays.getDate() - (churnDaysToShow - 1))
+  windowOfDays.setDate(windowOfDays.getDate() - +churnDaysToShow)
 
   const filteredItems = items.filter((item) => {
     const itemDate = new Date(item.dateReListed)
